d2e380a6734973918dfdebde888f7cf326ab48c9
张靖 <zhangjin6793@hotmail.com>
2022-05-30 10:51:29 +0800

    备份

--- PLCSimulation/Program.cs
+++ PLCSimulation/Program.cs
@@ -9,9 +9,25 @@ namespace PLCSimulation
     class Program
     {
         public static Socket socket;
-        public static Server[] servers;
+        public static Server servers;
         static void Main(string[] args)
         {
+            Console.WriteLine("Run...");
+            Console.WriteLine("请输入：同步 t / 异步 y");
+
+            bool tb = false;
+
+            string v = Console.ReadLine();
+            if ("t" == v)
+            {
+                tb = true;
+                Console.WriteLine("已选同步");
+            }
+            else if ("y" == v)
+            {
+                tb = false;
+                Console.WriteLine("已选异步");
+            }
             var localIp = "127.0.0.1";
             var localPort = 12289;
             byte[] buffer = new byte[14];
@@ -20,7 +36,7 @@ namespace PLCSimulation
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, localPort);
             socket.Bind(ipEndPoint);
-            setSocket(3);
+            setSocket(1);
             //socket.Listen(3);
 
             //Server[] servers = new Server[3];
@@ -33,46 +49,72 @@ namespace PLCSimulation
                 Thread thread = new Thread(threadStart);
                 thread.Start();
             }*/
-            
+            if (tb)
+            {
+                Console.WriteLine("同步工作开始");
+            }
+            else
+            {
+                Console.WriteLine("异步工作开始");
+            }
 
             while (true)
-            {
+            {                
                 var readLine = Console.ReadLine();
-                if (readLine == "10")
+                if (tb)
                 {
-                    servers[0].cmd = "11OK0000";
-                }else if (readLine == "1")
-                {
-                    servers[0].cmd = "11OK0001";
-                }else if (readLine == "20")
-                {
-                    servers[1].cmd = "11OK0000";
-                }else if (readLine == "2")
-                {
-                    servers[1].cmd = "11OK0001";
-                }else if (readLine == "30")
-                {
-                    servers[2].cmd = "11OK0000";
-                }else if (readLine == "3")
-                {
-                    servers[2].cmd = "11OK0001";
+                    if (readLine == "1")
+                    {
+                        Server.cmdTB = "11OK0001\r\n";
+                        Console.WriteLine(Server.cmdTB);
+                    }
+                    else if (readLine == "2")
+                    {
+                        Server.cmdTB = "11OK0002\r\n";
+                    }
+                    else if (readLine == "3")
+                    {
+                        Server.cmdTB = "11OK0003\r\n";
+                    }
+                    else
+                    {
+                        Server.cmdTB = "11OK0000\r\n";
+                    }
                 }
+                else
+                {
+                    if (readLine.Contains("1"))
+                    {
+                        Server.cmd1 = "11OK0001\r\n";
+                    }
+                    if (readLine.Contains("2"))
+                    {
+                        Server.cmd2 = "11OK0001\r\n";
+                    }
+                    if (readLine.Contains("3"))
+                    {
+                        Server.cmd3 = "11OK0001\r\n";
+                    }
+                    if (readLine == "0")
+                    {
+                        Server.cmd1 = "11OK0000\r\n";
+                        Server.cmd2 = "11OK0000\r\n";
+                        Server.cmd3 = "11OK0000\r\n";
+                    }
+                }                
             }
         }
 
         public static void setSocket(int num)
         {
             socket.Listen(num);
-            servers = new Server[num];
-            for (int i = 0; i < num; i++)
-            {
-                var clientSocket = socket.Accept();
-                servers[i] = new Server(clientSocket);
+            var clientSocket = socket.Accept();
+            servers = new Server(clientSocket);
 
-                ThreadStart threadStart = new ThreadStart(servers[i].plcSim);
-                Thread thread = new Thread(threadStart);
-                thread.Start();
-            }
+            ThreadStart threadStart = new ThreadStart(servers.plcSim);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            Console.WriteLine("监听结束");
         }
     }
 }
--- PLCSimulation/server.cs
+++ PLCSimulation/server.cs
@@ -9,8 +9,12 @@ namespace PLCSimulation
     public class Server
     {
         public Socket socket;
-        public string cmd = "11OK0000\r\n";
+        public static string cmdTB = "11OK0000\r\n";
         public byte[] buffer = new byte[1024];
+
+        public static string cmd1 = "11OK0000\r\n";
+        public static string cmd2 = "11OK0000\r\n";
+        public static string cmd3 = "11OK0000\r\n";
 
         public Server(Socket socket)
         {
@@ -19,7 +23,132 @@ namespace PLCSimulation
 
         public void plcSim()
         {
-            serverThread(socket, cmd);
+            //serverThread(socket, cmdTB);
+            ServerThreadTB(socket);
+        }
+
+        private void ServerThreadTB(Socket socket)
+        {
+            string lastStr = "";
+            while (true)
+            {
+                lock (this)
+                {
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        buffer[i] = 0;
+                    }
+
+                    var receive = socket.Receive(buffer);
+                    if (receive != 0)
+                    {
+                        var str = Encoding.ASCII.GetString(buffer);
+                        var indexOf = str.IndexOf('\r');
+                        if (indexOf != -1)
+                        {
+                            str = str.Substring(0, indexOf);
+                        }
+                        else
+                        {
+                            str = str.Substring(0, receive);
+                        }
+                        //Console.WriteLine(str);
+                        if (str == "01WRDD6030 01")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes(cmd1));
+                        }
+                        if (str == "01WRDD8030 01")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes(cmd2));
+                        }
+                        if (str == "01WRDD10030 01")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes(cmd3));
+                        }
+                        if (str == "01WRDD5950 01")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes(cmdTB));
+                        }
+                        if (str == "01WWRD6030 01 0000")
+                        {
+                            cmd1 = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("1-Reset");
+                        }
+                        if (str == "01WWRD8030 01 0000")
+                        {
+                            cmd2 = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("2-Reset");
+                        }
+                        if (str == "01WWRD10030 01 0000")
+                        {
+                            cmd3 = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("3-Reset");
+                        }
+                        if (str == "01WWRD5950 01 0000")
+                        {
+                            cmdTB = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("TB-Reset");
+                        }
+                        if (str == "01WWRD6032 01 0001")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("1-OK");
+                        }
+                        if (str == "01WWRD8032 01 0001")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("2-OK");
+                        }
+                        if (str == "01WWRD10032 01 0001")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("3-OK");
+                        }
+                        if (str == "01WWRD6032 01 0002")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("1-NG");
+                        }
+                        if (str == "01WWRD8032 01 0002")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("2-NG");
+                        }
+                        if (str == "01WWRD10032 01 0002")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("3-NG");
+                        }
+                        if (str == "01WWRD5952 01 0001")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("TB-1");
+                        }
+                        if (str == "01WWRD5952 01 0002")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("TB-2");
+                        }
+                        if (str == "01WWRD5952 01 0003")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("TB-3");
+                        }
+                        if (str == "01WWRD5952 01 0004")
+                        {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
+                            Console.WriteLine("TB-4");
+                        }
+                        lastStr = str;
+                    }
+
+                    Thread.Sleep(10);
+                }
+            }
         }
 
         private void serverThread(Socket socket, string cmdStr)
@@ -47,68 +176,84 @@ namespace PLCSimulation
                         {
                             str = str.Substring(0, receive);
                         }
+                        //Console.WriteLine(str);
                         if (str == "01WRDD6030 01" || str == "01WRDD8030 01" || str == "01WRDD10030 01" || str == "01WRDD5950 01")
                         {
-                            socket.Send(Encoding.ASCII.GetBytes(cmd));
+                            //Console.WriteLine("Return "+ cmd);
+                            socket.Send(Encoding.ASCII.GetBytes(cmdTB));
                         }
                         if (str == "01WWRD6030 01 0000")
                         {
-                            cmd = "11OK0000\r\n";
+                            cmdTB = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("1-Reset");
                         }
                         if (str == "01WWRD8030 01 0000")
                         {
-                            cmd = "11OK0000\r\n";
+                            cmdTB = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("2-Reset");
                         }
                         if (str == "01WWRD10030 01 0000")
                         {
-                            cmd = "11OK0000\r\n";
+                            cmdTB = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("3-Reset");
                         }
                         if (str == "01WWRD5950 01 0000")
                         {
-                            cmd = "11OK0000\r\n";
+                            cmdTB = "11OK0000\r\n";
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("TB-Reset");
                         }
                         if (str == "01WWRD6032 01 0001")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("1-OK");
                         }
                         if (str == "01WWRD8032 01 0001")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("2-OK");
                         }
                         if (str == "01WWRD10032 01 0001")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("3-OK");
                         }
                         if (str == "01WWRD6032 01 0002")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("1-NG");
                         }
                         if (str == "01WWRD8032 01 0002")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("2-NG");
                         }
                         if (str == "01WWRD10032 01 0002")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("3-NG");
                         }
                         if (str == "01WWRD5952 01 0001")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("TB-1");
                         }
                         if (str == "01WWRD5952 01 0002")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("TB-2");
                         }
                         if (str == "01WWRD5952 01 0003")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("TB-3");
                         }
                         if (str == "01WWRD5952 01 0004")
                         {
+                            socket.Send(Encoding.ASCII.GetBytes("11OK\r\n"));
                             Console.WriteLine("TB-4");
                         }
                         lastStr = str;
